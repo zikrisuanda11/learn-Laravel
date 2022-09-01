dc2604fe009305d24d66e03391ed07b50ebe7a67
zikrisuanda11 <zikrisuanda998@gmail.com>
2022-09-01 17:36:14 +0800

    migration

--- database/migrations/2022_08_26_195549_create_parents_table.php
+++ database/migrations/2022_08_26_195549_create_parents_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->string('lname', 45);
             $table->string('phone', 15);
             $table->date('date_of_birth')->nullable();
-            $table->boolean('status');
+            $table->string('status')->default('parent');
             $table->timestamps();
         });
     }
